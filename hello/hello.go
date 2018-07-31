30faf5b2c1cdb753510e7f93fbf7b373d01e15a0
d-bean <d-bean@ex-em.com>
2018-07-31 09:19:49 +0900

    fmtgo test

--- hello/hello.go
+++ hello/hello.go
@@ -1,6 +1,15 @@
 package main
-import "fmt"
-func main(){
-	fmt.Printf("hello, world3\n")
+
+import (
+	"fmt"
+	"github.com/leb9882/stringutil"
+)
+
+type T struct {
+	name  string // name of the object
+	value int    // its value
 }
 
+func main() {
+	fmt.Printf(stringutil.Reverse("hello, world3\n"))
+}
